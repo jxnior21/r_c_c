31d116fca9f7a57296886f8f0a954344d26ecab7
Angel Lopez <angelyoutube@hotmail.com>
2018-03-21 12:40:13 -0400

    Added capacity and visible columns

--- db/migrate/20180106192953_create_events.rb
+++ db/migrate/20180106192953_create_events.rb
@@ -3,8 +3,10 @@ class CreateEvents < ActiveRecord::Migration[5.1]
     create_table :events do |t|
       t.string 'title', :default => 'Evento', :null => false
       t.string 'date', :default => '00/00/00', :null => true, :limit => 8
-      t.string 'description', :null => false
+      t.text 'description', :null => false
       t.string 'price', :default => '00.00', :null => false
+      t.integer 'capacity', :default => 0, :null => false
+      t.boolean 'visible', :default => false, :null => false
       t.timestamps
     end
   end
