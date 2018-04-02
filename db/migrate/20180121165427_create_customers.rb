85b0f0e9e82a8871f90fc7e5c09112c025ea33dc
Angel Lopez <angelyoutube@hotmail.com>
2018-04-01 21:34:19 -0400

    Added email column to customer table

--- db/migrate/20180121165427_create_customers.rb
+++ db/migrate/20180121165427_create_customers.rb
@@ -4,6 +4,7 @@ class CreateCustomers < ActiveRecord::Migration[5.1]
       t.string 'first_name', :null => false
       t.string 'last_name', :null => false
       t.string 'telephone', :null => false
+      t.string 'email'
       t.string 'address1', :null => false
       t.string 'city', :null => false
       t.string 'state', :null => false
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180121165427) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "telephone", null: false
+    t.string "email"
     t.string "address1", null: false
     t.string "city", null: false
     t.string "state", null: false
